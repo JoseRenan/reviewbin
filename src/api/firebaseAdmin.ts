1139ebb16e7e90b7d307fb54913bdfdd8cb7c121
José Renan <jose.renan@splab.ufcg.edu.br>
2021-04-16 01:27:51 -0300

    Fix firebase admin configuration to suport deploying from git

--- src/api/firebaseAdmin.ts
+++ src/api/firebaseAdmin.ts
@@ -1,9 +1,16 @@
 import * as admin from 'firebase-admin'
 
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+}
+
 if (admin.apps.length === 0) {
   admin.initializeApp({
-    credential: admin.credential.cert(require('../serviceAccount.json')),
+    credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   })
 }
 
